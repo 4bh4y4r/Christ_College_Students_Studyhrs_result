f05de8ae87e1a2a7ea89f76e0530a4c68e76e68b
AbhayAR <143073396+4bh4y4r@users.noreply.github.com>
2026-09-17 11:37:25 +0530

    Fix model file name spacing in load function

--- Students_StudyHrs_Result.py
+++ Students_StudyHrs_Result.py
@@ -3,7 +3,7 @@ import streamlit as st
 import joblib
 import pandas as pd
 
-model = joblib.load("logistic_regression_StudyHrs_model(1).pkl")
+model = joblib.load("logistic_regression_StudyHrs_model (1).pkl")
 
 st.title("Student Pass/Fail Prediction")
 
